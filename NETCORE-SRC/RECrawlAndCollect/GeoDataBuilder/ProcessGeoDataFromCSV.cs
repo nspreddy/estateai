00746876dca801b5cdb2154f6667f801d29110bb
Nagareddy Reddy <nagareddy526@gmail.com>
2021-07-08 13:59:06 -0700

    Adding code create geoDBs

---
+++ NETCORE-SRC/RECrawlAndCollect/GeoDataBuilder/ProcessGeoDataFromCSV.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace GeoDataBuilder
+{
+    class ProcessGeoDataFromCSV
+    {
+        private const int NUMBEROFENTITIES = 15;        
+        private const int ZIP_COL_INDEX = 0;
+        private const int CITY_COL_INDEX = 3;
+        private const int STATE_COL_INDEX = 6;
+        private const int COUNTY_COL_INDEX = 7;
+        private const int COUNTRY_COL_INDEX = 11;
+        private string inputFileName;
+        private string outputFileName;
+
+
+        private const string CSV_REGEX = @"(?:,|^)([^"",]+|""(?:[^""]|"""")*"")?";
+        
+
+        #region PRIVATE_METHODS
+
+        private bool ProcessRow(string row)
+        {
+            bool returnValue = false;
+
+            try
+            {
+                var regExCollections = Regex.Matches(row, CSV_REGEX);
+                if( regExCollections != null && regExCollections.Count == NUMBEROFENTITIES)
+                {
+                    if( regExCollections[0].Value.Trim()== "zip")
+                    {
+                        Console.WriteLine("Skipping header row");
+                    }
+                    else
+                    {
+                        var state = regExCollections[STATE_COL_INDEX]?.Value.TrimStart(',').Trim();
+                        var county  = regExCollections[COUNTY_COL_INDEX]?.Value.TrimStart(',').Trim('\"').Trim();
+                        var city    = regExCollections[CITY_COL_INDEX]?.Value.TrimStart(',').Trim();
+                        var zipcode = regExCollections[ZIP_COL_INDEX]?.Value.TrimStart(',').Trim();
+
+                        Console.WriteLine($" State: {state}, County:{county}, City: {city} , ZipCode: {zipcode}");
+                    }                   
+                }
+                else
+                {
+                    Console.WriteLine($" Wrong Format Row {row}");
+                }               
+
+            }catch (Exception ex)
+            {
+                Console.WriteLine($"Exception : {ex.Message}");
+            }
+
+            return returnValue;
+        }
+
+        #endregion
+
+
+        public ProcessGeoDataFromCSV( string inputfile, string outputfile)
+        {
+            inputFileName = inputfile;
+            outputFileName = outputfile;
+        }
+
+        public bool Process()
+        {
+            bool returnVal = false;
+
+            if (!string.IsNullOrEmpty(inputFileName) && !string.IsNullOrEmpty(outputFileName))
+            {
+                // Start reading input file .
+                try
+                {
+                    using (StreamReader sr = new StreamReader(inputFileName))
+                    {
+                        string line;
+                        int failedRows=0;
+                        // Read and display lines from the file until the end of
+                        // the file is reached.
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            if( !ProcessRow(line))
+                            {
+                                ++failedRows;                                
+                            }
+                        }
+                        var outputStr =  failedRows > 0 ? $" Failed ROWS: {failedRows}" : $"processed with no errors";
+                        Console.WriteLine(outputStr);
+                    }
+
+                    returnVal = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Exception While Processing {ex.Message}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"FileNames null or empty.. I:{inputFileName} O:{outputFileName}");
+            }
+            
+
+            return returnVal;
+        }
+
+       
+    }
+}
---
+++ NETCORE-SRC/RECrawlAndCollect/GeoDataBuilder/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.Extensions.CommandLineUtils;
+
+
+namespace GeoDataBuilder
+{
+    class Program
+    {
+        private const string URLOPTION         = "-u|--urls <value>";
+        private const string CFGFILEOPTION     = "-c|--file <value>";
+        private const string OUTPUTFILEOPTION  = "-o|--file <value>";
+
+        private const string HELP = "-? | -h | --help";
+
+        private const string VERSIONOPTION = "-v|--version";
+        private const string VERSION = "0.1";
+
+        private const string DESC = "App to build State,County,City,ZipCode into proper JSON (common schema)";
+        static void Main(string[] args)
+        {
+            var execName = System.AppDomain.CurrentDomain.FriendlyName;
+            var app = new CommandLineApplication();
+            app.Name = execName;
+            app.Description = DESC;
+
+            #region HELP_VERSION
+            app.HelpOption(HELP);
+            app.VersionOption(VERSIONOPTION, () =>
+            {
+
+                return string.Format($"Version {VERSION}");
+            });
+            #endregion
+
+            #region OPTIONS_ACTIONS
+            var configFileOption = app.Option(CFGFILEOPTION, "Configuration file(CSV)", CommandOptionType.SingleValue);
+            var urlsOption       = app.Option(URLOPTION, "URL to Crawl GEO DB", CommandOptionType.SingleValue);
+            var outputFileOption = app.Option(OUTPUTFILEOPTION, "Configuration file(CSV)", CommandOptionType.SingleValue);
+
+            #endregion
+
+            #region PROCESSING_CODE
+            app.OnExecute(() =>
+            {
+                bool optionExecuted = false;
+                bool optionsChecksPassed = false;
+                string outputFile="";
+                int returnVal = 0;
+
+                if (outputFileOption.HasValue())
+                {
+                    outputFile = outputFileOption.Value();
+                    optionsChecksPassed= !string.IsNullOrEmpty(outputFile);
+                }
+                else
+                {
+                    Console.WriteLine("Mandatory param  for output file name  missing");
+                    Console.WriteLine($" HELP: {execName} {HELP}");
+                    returnVal = -1;
+                }
+
+                if (optionsChecksPassed)
+                {
+
+                    if (urlsOption.HasValue())
+                    {
+                        // Let us crawl thoses URLs.
+                        var urls = urlsOption.Values;
+                        // TBD Crawl and write JSON data
+                        optionExecuted = true;
+                    }
+
+                    if (configFileOption.HasValue())
+                    {
+                        var configFile = configFileOption.Value();
+                        Console.WriteLine($"Parsing file {configFile} for Geo Data nad output will be written to {outputFile}");
+                        var geoDataProcessor = new ProcessGeoDataFromCSV(configFile, outputFile);
+                        if( geoDataProcessor.Process())
+                        {
+                            Console.WriteLine($"Processed data sucessfully, please see outputfile {outputFile}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Failed to process {configFile}");
+                            returnVal = -1;
+                        }
+                        optionExecuted = true;
+
+                    }
+                }
+
+                if (!optionExecuted)
+                {
+                    Console.WriteLine($" OPT-HELP: {execName} {HELP}");
+                    returnVal = -1;
+                }
+                return returnVal;
+            });
+
+            #endregion
+
+            try
+            {
+                if (args.Length == 0)
+                {
+                    Console.WriteLine($" HELP: {execName} {HELP}");
+                }
+                else
+                {
+                    // Setup as needed
+                    var result = app.Execute(args);
+                    Console.WriteLine($"Result of {execName} Execute .. {result}");
+                }
+            }
+            catch (CommandParsingException)
+            {
+                Console.WriteLine($" HELP: {execName} {HELP}");
+            }
+        }
+    }
+}
