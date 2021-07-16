8dfb91d2fca229d395d6b87133624693bb32aa79
Nagareddy Reddy <nagareddy526@gmail.com>
2021-07-15 19:44:27 -0700

    Good frame work so far, we are ready to crawl in few steps.

--- NETCORE-SRC/RECrawlAndCollect/PropAnalyzer/AnalysisHelper.cs
+++ NETCORE-SRC/RECrawlAndCollect/PropAnalyzer/AnalysisHelper.cs
@@ -7,6 +7,7 @@ using DataModels;
 using CommonAndUtils;
 using RedfinUtils;
 using CrawlerLib;
+using System.IO;
 
 namespace PropAnalyzer
 {
@@ -49,40 +50,84 @@ namespace PropAnalyzer
         public bool PrepListOfProperyHtmlPages2Extract()
         {
             bool returnValue = false;
-
-            var stateObject = GeoData.DefaultNation.GetState(ProcessorCfg.State);
-            if (stateObject != null)
+            try
             {
-                // get cities and zipcodes in Hash set ( look up is faster.. O(1) incase of unique hash keys
-                UpdateHashSetWithCityList(ProcessorCfg.CityList);
-                UpdateHashSetWithZipCodeList(ProcessorCfg.ZipCodeList);
-
-                // convert counties to cities
-                foreach (var county in ProcessorCfg.CountyList)
+                var stateObject = GeoData.DefaultNation.GetState(ProcessorCfg.State);
+                if (stateObject != null)
                 {
-                    var listofCities = stateObject.GetListofCitiesInaCounty(county);
-                    UpdateHashSetWithCityList(listofCities);
+                    // get cities and zipcodes in Hash set ( look up is faster.. O(1) incase of unique hash keys
+                    UpdateHashSetWithCityList(ProcessorCfg.CityList);
+                    UpdateHashSetWithZipCodeList(ProcessorCfg.ZipCodeList);
+
+                    // convert counties to cities
+                    foreach (var county in ProcessorCfg.CountyList)
+                    {
+                        var listofCities = stateObject.GetListofCitiesInaCounty(county);
+                        UpdateHashSetWithCityList(listofCities);
+                    }
+
+                    var dirPath = stateObject.GetDirForPropertyData(InDir, DateDir, ProcessorCfg.JobName);
+
+                    // Typical Prop Filename : wa_auburn_12269_31900-104th-Ave-SE-98092_unit-C104_98092.html
+
+                    /*
+                     * 1. Enumurate Directory 
+                     * 2. For each file, extract City,Zip Code from filename
+                     * 3. Add to list based on allowed city,zip list 
+                     * 
+                     */
+                    var htmlFiles = Directory.EnumerateFiles(dirPath, "*.html", SearchOption.TopDirectoryOnly);
+
+                    if( htmlFiles != null && htmlFiles.Count() > 0)
+                    {
+                        foreach( var htmlFile in htmlFiles)
+                        {
+                            var filename = Path.GetFileNameWithoutExtension(htmlFile);
+                            if(!string.IsNullOrEmpty(filename))
+                            {
+                                // let us get tokens to understand the eligibility to extract content. 
+                                var tokens = filename.Split("_");
+                                var city = tokens[1];
+                                var zipcode = tokens[4];
+
+                                bool isPropCrawlable = false;
+
+                                if (!string.IsNullOrEmpty(city) && Cities2Crawl.Contains(city))
+                                {
+                                    isPropCrawlable = true;
+                                }
+                                else if (!string.IsNullOrEmpty(zipcode) && ZipCodes2Crawl.Contains(zipcode))
+                                {
+                                    isPropCrawlable = true;
+                                }
+
+                                if (isPropCrawlable)
+                                {
+                                    var propKey = $"{city}_{zipcode}";
+                                    AddPropUrl(propKey, htmlFile);
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Unable to enumurate HTML files in dir:{dirPath}");
+                    }
+
                 }
-                
-                var dirPath = stateObject.GetDirForPropertyData(InDir, DateDir, ProcessorCfg.JobName);
-
-                /*
-                 * 1. Enumurate Directory 
-                 * 2. For each file, extract City,Zip Code from filename
-                 * 3. Add to list based on allowed city,zip list 
-                 * 
-                 */
-
-
+                else
+                {
+                    Console.WriteLine($"State:{ProcessorCfg.State} not found in the given GeoData");
+                }
             }
-            else
+            catch(Exception ex)
             {
-                Console.WriteLine($"State:{ProcessorCfg.State} not found in the given GeoData");
+
             }
             return returnValue;
         }
 
-        private bool PrepListOfStatsHtmlPages2Extract()
+        public  bool PrepListOfStatsHtmlPages2Extract()
         {
             bool returnValue = false;
 
@@ -97,6 +142,58 @@ namespace PropAnalyzer
                 // ToDo
                 // 1. Enumurate files, read file name and determine county,city or zip code stats file. 
                 // 2. Short list them based on input list 
+                // ex: file names wa_citystats_redmond.html, wa_countystats_king.html, wa_zipcodestats_98075.html 
+                var htmlFiles = Directory.EnumerateFiles(dirPath, "*.html", SearchOption.TopDirectoryOnly);
+
+                if (htmlFiles != null && htmlFiles.Count() > 0)
+                {
+                    foreach (var htmlFile in htmlFiles)
+                    {
+                        var filename = Path.GetFileNameWithoutExtension(htmlFile);
+                        if (!string.IsNullOrEmpty(filename))
+                        {
+                            // let us get tokens to understand the eligibility to extract content. 
+                            var tokens = filename.Split("_");
+                            var state = tokens[1];
+                            var type = tokens[1];
+                            var valueBasedonType = tokens[2];
+
+                            bool isPropCrawlable = false;
+
+                            switch (type)
+                            {
+                                case "citystats":
+                                    if (!string.IsNullOrEmpty(valueBasedonType) && Cities2Crawl.Contains(valueBasedonType))
+                                    {
+                                        isPropCrawlable = true;
+                                    }
+                                    break;
+                                case "countystats":
+                                    if (!string.IsNullOrEmpty(valueBasedonType) && Counties2Crawl.Contains(valueBasedonType))
+                                    {
+                                        isPropCrawlable = true;
+                                    }
+                                    break;
+                                case "zipcodestats":
+                                    if (!string.IsNullOrEmpty(valueBasedonType) && ZipCodes2Crawl.Contains(valueBasedonType))
+                                    {
+                                        isPropCrawlable = true;
+                                    }
+                                    break;
+                            }                            
+
+                            if (isPropCrawlable)
+                            {
+                                var propKey = $"{type}_{valueBasedonType}";
+                                AddPropUrl(propKey, htmlFile);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Unable to enumurate HTML files in dir:{dirPath}");
+                }
 
 
                 returnValue = true;
@@ -161,6 +258,15 @@ namespace PropAnalyzer
                 }
             }
         }
+
+        private void AddPropUrl(string key, string url)
+        {
+            if (!propertyFiles2Analyze.ContainsKey(key))
+            {
+                propertyFiles2Analyze.Add(key, url);
+            }
+        }
+
         #endregion
     }
 
